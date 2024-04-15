3ee0e54d148ebb7d7572ae73da4c34c0ad6c5f70
Stefan Anevski <stefan.anevski@one-inside.com>
2024-04-15 21:44:30 +0200

    T2C-38: update the background image on register page

--- coursesApp/src/Pages/Register/RegisterPage.jsx
+++ coursesApp/src/Pages/Register/RegisterPage.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import RegisterForm from '../../Components/Register/RegisterForm';
+import background from "../../assets/background.png";
 
 const RegisterPage = () => {
   return (
@@ -14,7 +14,7 @@ const RegisterPage = () => {
         </div>
       </div>
       <div className="w-full md:w-1/2 flex items-center justify-center">
-        <img src="/placeholder.svg" alt="placeholder-image" className="max-h-full max-w-full" />
+        <img src={background} alt="background-image" className="max-h-full max-w-full" />
       </div>
     </div>
   );
