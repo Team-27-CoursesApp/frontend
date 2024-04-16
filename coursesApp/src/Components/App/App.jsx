e3459ca2b0147b1fb7c29382281df583e719ebbd
Nace Gjorgjievski <nace.gorgievski123@gmail.com>
2024-04-16 17:20:47 +0200

    Create cart page

--- coursesApp/src/Components/App/App.jsx
+++ coursesApp/src/Components/App/App.jsx
@@ -1,21 +1,19 @@
-import LoginPage from "../../Pages/Login/LoginPage"
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import LoginPage from "../../Pages/Login/LoginPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegisterPage from "../../Pages/Register/RegisterPage";
 import AboutUs from "../../Pages/AboutUs/AboutUs";
 import HomePage from "../../Pages/Home/HomePage";
+import CartPage from "../../Pages/Cart/CartPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/aboutUs" element={<AboutUs />} />
+        <Route path="/cart" element={<CartPage />} />
       </Routes>
     </BrowserRouter>
   );
