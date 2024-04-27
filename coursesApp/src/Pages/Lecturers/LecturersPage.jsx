ffb2db036147b2793c1532d6c5baab51279280e6
krstevskiDarko <darko_krstevski@icloud.com>
2024-04-27 18:12:20 +0200

    Added Hardcoded Lecturers; Fixed Layout. Added Lecturer as a component;

--- coursesApp/src/Pages/Lecturers/LecturersPage.jsx
+++ coursesApp/src/Pages/Lecturers/LecturersPage.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 
-const Lecturers = () => {
+const LecturersPage = () => {
     return (
         <Lecturer />
     )
 }
 
-export default Lecturers;
+export default LecturersPage;
