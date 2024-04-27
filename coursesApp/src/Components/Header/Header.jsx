0ab012aa8a0a6719ded90873947d853657cf31cf
krstevskiDarko <darko_krstevski@icloud.com>
2024-04-27 16:39:39 +0200

    Refactoring Header Component; Adding Navigation on all pages

--- coursesApp/src/Components/Header/Header.jsx
+++ coursesApp/src/Components/Header/Header.jsx
@@ -1,56 +1,72 @@
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 import color_logo_no_background from '../../assets/LogoInfo/Logo Files/For Web/png/color_logo_no_background.png';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import FormatAlignJustifyIcon from '@mui/icons-material/FormatAlignJustify';
 
-const Header = () => {
+const Header = (props) => {
+    const [showHeader, setShowHeader] = useState(false);
+
     return (
         <header>
-            <nav className="bg-white light:bg-gray-900 w-full z-20 top-0 start-0 border-b border-gray-200 light:border-gray-600">
-                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                    <a href="/" className="flex items-center space-x-3 ">
-                        <img src={color_logo_no_background} className="h-8" alt="SkillSphere logo" />
-                    </a>
-                    <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                        <Link to="/shoppingcart">
-                            <ShoppingCartIcon href="/" sx={{ color: "#1560BD" }} />
-                        </Link>
+            <nav
+                className="bg-white light:bg-gray-900 w-full z-20 top-0 start-0 border-b border-gray-200 light:border-gray-600">
+                <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
+                    <div className="flex items-center space-x-3">
+                        <a href="/" className="flex items-center space-x-3">
+                            <img src={color_logo_no_background} className="h-8" alt="SkillSphere logo"/>
+                        </a>
+                        <button
+                            type="button"
+                            className="text-gray-900 block flex items-center justify-center lg:hidden"
+                            onClick={() => setShowHeader(!showHeader)}
+                        >
+                            <FormatAlignJustifyIcon />
+                        </button>
                     </div>
-                    <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                        <Link to="/login">
-                            <button
-                                type="button"
-                                className="text-white bg-gradient-to-r from-blue-800 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-                                Login
-                            </button>
-                        </Link>
-                        <Link to="/register">
-                            <button
-                                type="button"
-                                className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-black-900 rounded-lg group bg-gradient-to-br from-blue-500 to-blue-900 group-hover:from-bluee-500 group-hover:to-blue-900 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 ">
-                            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white light:bg-gray-900 rounded-md group-hover:bg-opacity-0 text-blue-900 hover:text-white">
-                              Register
-                            </span>
-                            </button>
-                        </Link>
-                    </div>
-                    <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-                        <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-light-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white light:bg-gray-800 md:light:bg-gray-900 light:border-gray-700">
-                            <li>
-                                <a href="/" className="block py-2 px-3 bg-light-700 rounded md:bg-transparent md:text-blue-700 text-transform uppercase md:p-0 md:dark:text-blue-500">Почетна</a>
+                    <div
+                        className={`lg:flex lg:order-2 space-x-3 lg:space-x-0 rtl:space-x-reverse ${showHeader ? 'flex' : 'hidden'}`}>
+                        <ul className="flex flex-col p-4 lg:p-0 font-medium border border-gray-100 rounded-lg bg-light-50 lg:space-x-8 rtl:space-x-reverse lg:flex-row md:mt-0 lg:border-0 lg:bg-white light:bg-gray-800 lg:light:bg-gray-900 light:border-gray-700">
+                            <li className={`${props.page === 'Home' ? 'text-blue-500' : 'text-gray-900'}`}>
+                                <a href="/"
+                                   className="block py-2 px-3  rounded hover:bg-gray-100 lg:hover:bg-transparent  lg:hover:text-blue-700 lg:p-0 lg:dark:hover:text-blue-500 text-transform uppercase">Почетна</a>
+                            </li>
+                            <li className={`${props.page === 'AboutUs' ? 'text-blue-500' : 'text-gray-900'}`}>
+                                <a href="/aboutus"
+                                   className="block py-2 px-3  rounded hover:bg-gray-100 lg:hover:bg-transparent lg:hover:text-blue-700 lg:p-0 lg:dark:hover:text-blue-500 text-transform uppercase">
+                                    За нас</a>
+                            </li>
+                            <li className={`${props.page === 'Courses' ? 'text-blue-500' : 'text-gray-900'}`}>
+                                <a href="/courses"
+                                   className="block py-2 px-3  rounded hover:bg-gray-100 lg:hover:bg-transparent  lg:hover:text-blue-700 lg:p-0 lg:dark:hover:text-blue-500 text-transform uppercase">Курсеви</a>
                             </li>
                             <li>
-                                <a href="/aboutus" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-black dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-transform uppercase">За нас</a>
-                            </li>
-                            <li>
-                                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-black dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-transform uppercase">Курсеви</a>
-                            </li>
-                            <li>
-                                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-black dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-transform uppercase">Предавачи</a>
-                            </li>
-                            <li>
-                                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-black dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-transform uppercase">Контакт</a>
+                                <a href="#"
+                                   className="block py-2 px-3  rounded hover:bg-gray-100 lg:hover:bg-transparent  lg:hover:text-blue-700 lg:p-0 lg:dark:hover:text-blue-500 text-transform uppercase">Предавачи</a>
                             </li>
                         </ul>
+                    </div>
+                    <div
+                        className={`md:flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse`}>
+                        <div className="flex space-x-3 align-center items-center">
+                            <Link to="/login">
+                                <button
+                                    type="button"
+                                    className="text-white bg-gradient-to-r from-blue-800 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2">
+                                    Login
+                                </button>
+                            </Link>
+                            <Link to="/register">
+                                <button
+                                    type="button"
+                                    className="text-white bg-gradient-to-r from-blue-800 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2">
+                                    Register
+                                </button>
+                            </Link>
+                            <Link to="/shoppingcart">
+                                <ShoppingCartIcon sx={{color: "#1560BD"}}/>
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </nav>
--- coursesApp/src/Pages/Register/RegisterPage.jsx
+++ coursesApp/src/Pages/Register/RegisterPage.jsx
@@ -1,23 +1,29 @@
 import RegisterForm from '../../Components/Register/RegisterForm';
 import background from "../../assets/background.png";
+import Header from "../../Components/Header/Header"
+import Footer from "../../Components/Footer/Footer"
 
 const RegisterPage = () => {
-  return (
-    <div className="flex flex-col md:flex-row h-screen">
-      <div className="w-full md:w-1/2 flex flex-col items-center justify-center md:pb-0 pb-8">
-        <div className="w-full max-w-md"> 
-          <h1 className="mb-3 text-blue-500 font-bold text-3xl">
-            <span className="block">Сo SkillSphere до ново и</span>
-            <span className="block">поголемо знаење!</span>
-          </h1>
-          <RegisterForm />
-        </div>
-      </div>
-      <div className="w-full md:w-1/2 flex items-center justify-center">
-        <img src={background} alt="background-image" className="max-h-full max-w-full" />
-      </div>
-    </div>
-  );
+    return (
+        <>
+            <Header/>
+            <div className="flex flex-col md:flex-row h-screen">
+                <div className="w-full md:w-1/2 flex flex-col items-center justify-center md:pb-0 pb-8">
+                    <div className="w-full max-w-md">
+                        <h1 className="mb-3 text-blue-500 font-bold text-3xl">
+                            <span className="block">Сo SkillSphere до ново и</span>
+                            <span className="block">поголемо знаење!</span>
+                        </h1>
+                        <RegisterForm/>
+                    </div>
+                </div>
+                <div className="w-full md:w-1/2 flex items-center justify-center">
+                    <img src={background} alt="background-image" className="max-h-full max-w-full"/>
+                </div>
+            </div>
+            <Footer/>
+        </>
+    );
 };
 
 export default RegisterPage;
