7f00e51d8af4ad100ffdf2d46c287e90154e2a33
Stefan Anevski <anevskistefan11@gmail.com>
2024-04-28 19:49:06 +0200

    T2C-71: integrate newly created page to the routes

--- coursesApp/src/Components/App/App.jsx
+++ coursesApp/src/Components/App/App.jsx
@@ -8,6 +8,8 @@ import CartPage from "../../Pages/Cart/CartPage";
 import CategoriesPage from "../../Pages/Categories/CategoriesPage";
 import CourseListPage from "../../Pages/CourseList/CourseListPage";
 import CheckoutPage from "../../Pages/Checkout/CheckoutPage";
+import CourseDetailsOfABoughtCoursePage from "../../Pages/CourseDetailsOfABoughtCourse/CourseDetailsOfABoughtCoursePage";
+
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/aboutUs" element={<AboutUs />} />
         <Route path="/courseDetails" element={<CourseDetails />} />
+        <Route path="/courseDetails/:courseId" element={<CourseDetailsOfABoughtCoursePage />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/courses" element={<CategoriesPage />} />
         <Route path="/courses/:category" element={<CourseListPage />} />
