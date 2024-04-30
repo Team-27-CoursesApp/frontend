2d594ebb44aa7c1437719a0334dbb56d248d0ec0
Stefan Anevski <stefan.anevski@one-inside.com>
2024-04-30 18:44:51 +0200

    T2C-57: fix layout

--- coursesApp/src/Components/Header/Header.jsx
+++ coursesApp/src/Components/Header/Header.jsx
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 import color_logo_no_background from '../../assets/LogoInfo/Logo Files/For Web/png/color_logo_no_background.png';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import FormatAlignJustifyIcon from '@mui/icons-material/FormatAlignJustify';
+import Search from "../Search/Search";
 
 const Header = (props) => {
     const [showHeader, setShowHeader] = useState(false);
@@ -24,6 +25,7 @@ const Header = (props) => {
                             <FormatAlignJustifyIcon />
                         </button>
                     </div>
+
                     <div
                         className={`lg:flex lg:order-2 space-x-3 lg:space-x-0 rtl:space-x-reverse ${showHeader ? 'flex' : 'hidden'}`}>
                         <ul className="flex flex-col p-4 lg:p-0 font-medium border border-gray-100 rounded-lg bg-light-50 lg:space-x-8 rtl:space-x-reverse lg:flex-row md:mt-0 lg:border-0 lg:bg-white light:bg-gray-800 lg:light:bg-gray-900 light:border-gray-700">
@@ -46,9 +48,12 @@ const Header = (props) => {
                             </li>
                         </ul>
                     </div>
+
                     <div
                         className={`md:flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse`}>
-                        <div className="flex space-x-3 align-center items-center">
+                        <Search />
+
+                        <div className="flex space-x-3 align-center items-center pl-5">
                             <Link to="/login">
                                 <button
                                     type="button"
@@ -67,7 +72,9 @@ const Header = (props) => {
                                 <ShoppingCartIcon sx={{color: "#1560BD"}}/>
                             </Link>
                         </div>
+
                     </div>
+
                 </div>
             </nav>
         </header>
