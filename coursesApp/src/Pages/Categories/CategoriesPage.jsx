c72b0cfc62e92d44593be48982df979be8c1d4c0
Nace Gjorgjievski <nace.gorgievski123@gmail.com>
2024-04-30 22:48:15 +0200

    Added Categories Page Functionality

--- coursesApp/src/Pages/Categories/CategoriesPage.jsx
+++ coursesApp/src/Pages/Categories/CategoriesPage.jsx
@@ -1,39 +1,47 @@
-import {useState} from "react";
-import {useNavigate} from "react-router-dom";
-import Header from "../../Components/Header/Header"
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
-
+import axios from "axios";
 
 const CategoriesPage = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [categories, setCategories] = useState([]);
 
-    const [categories, setCategories] = useState([
-        "Вештачка интелигенција",
-        "Сајбер безбедност",
-        "Алгоритми и податочни структури",
-    ]);
-    return (
-        <>
-            <Header page={"Courses"}/>
-            <div className="flex flex-col items-center my-5">
-                {categories.map((category) => (
-                    <div
-                        onClick={() => navigate(`/courses/${category}`)}
-                        key={category}
-                        className="bg-gray-400 mt-5 h-36 w-11/12 md:w-4/5 lg:w-2/3 flex items-center justify-center rounded cursor-pointer"
-                    >
-                        <h1 className="lg:text-2xl md:text-xl text-lg text-center">
-                            Курсеви за{" "}
-                            <span className="text-blue-900 text-pretty font-bold">
-              {category}
-            </span>
-                        </h1>
-                    </div>
-                ))}
-            </div>
-            <Footer/>
-        </>
-    );
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get("/api/categories");
+        setCategories(data);
+      } catch (error) {
+        console.log("Error fetching categories");
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <Header page={"Courses"} />
+      <div className="flex flex-col items-center my-5">
+        {categories.map((category) => (
+          <div
+            onClick={() => navigate(`/courses/${category.slug}`)}
+            key={category.id}
+            className="bg-gray-400 mt-5 h-36 w-11/12 md:w-4/5 lg:w-2/3 flex items-center justify-center rounded cursor-pointer"
+          >
+            <h1 className="lg:text-2xl md:text-xl text-lg text-center">
+              Курсеви за{" "}
+              <span className="text-blue-900 text-pretty font-bold">
+                {category.name}
+              </span>
+            </h1>
+          </div>
+        ))}
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default CategoriesPage;
