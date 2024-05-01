f0ea460cb3d6eb7b42e0da7b4fcf907577445680
Nace Gjorgjievski <nace.gorgievski123@gmail.com>
2024-05-01 14:17:52 +0200

    Updated the Cart Page

--- coursesApp/src/Pages/Cart/CartPage.jsx
+++ coursesApp/src/Pages/Cart/CartPage.jsx
@@ -19,7 +19,12 @@ const CartPage = () => {
     <>
       <Header page={"Courses"} />
       <div className="h-screen bg-gray-100 pt-20">
-        <h1 className="mb-10 text-center text-2xl font-bold">Кошничка</h1>
+        <h1 className="mb-10 text-center text-2xl font-bold">
+          {" "}
+          {cartItems.reduce((a, c) => a + c.price, 0) == 0
+            ? "Кошничката е празна"
+            : "Кошничка"}
+        </h1>
         <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
           <div className="rounded-lg md:w-2/3">
             {cartItems.map((course) => (
@@ -67,25 +72,26 @@ const CartPage = () => {
               </div>
             ))}
           </div>
-
-          <div className="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
-            <div className="flex justify-between">
-              <p className="text-lg font-bold">Вкупно</p>
-              <div className="">
-                <p className="mb-1 text-lg font-bold">
-                  {cartItems.reduce((a, c) => a + c.price, 0)} ден
-                </p>
+          {cartItems.reduce((a, c) => a + c.price, 0) != 0 && (
+            <div className="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
+              <div className="flex justify-between">
+                <p className="text-lg font-bold">Вкупно</p>
+                <div className="">
+                  <p className="mb-1 text-lg font-bold">
+                    {cartItems.reduce((a, c) => a + c.price, 0)} ден
+                  </p>
+                </div>
               </div>
-            </div>
-            <hr className="my-4" />
+              <hr className="my-4" />
 
-            <button
-              onClick={() => navigate("/payment")}
-              className=" w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600"
-            >
-              Плати
-            </button>
-          </div>
+              <button
+                onClick={() => navigate("/payment")}
+                className=" w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600"
+              >
+                Плати
+              </button>
+            </div>
+          )}
         </div>
       </div>
       <Footer />
