c6dfbaba61f0d5ec009223064ab74907e49e186f
Nace Gjorgjievski <nace.gorgievski123@gmail.com>
2024-05-02 18:54:30 +0200

    Added Lecturers Page Functionality

--- coursesApp/src/Components/Lecturer/Lecturer.jsx
+++ coursesApp/src/Components/Lecturer/Lecturer.jsx
@@ -1,48 +1,46 @@
-import React from 'react'
-import {useNavigate} from "react-router-dom";
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
 import courseImage from "../../assets/course.png";
-import SmartDisplayIcon from '@mui/icons-material/SmartDisplay';
-import FileDownloadIcon from '@mui/icons-material/FileDownload';
-import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
+import SmartDisplayIcon from "@mui/icons-material/SmartDisplay";
+import FileDownloadIcon from "@mui/icons-material/FileDownload";
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
 
 const Lecturer = (props) => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  return (
+    <>
+      <div
+        onClick={() => navigate(`/profile/${props.lecturer.username}`)}
+        className="bg-gray-100 shadow-2xl rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700 dark:shadow-2xl cursor-pointer py-3 px-3"
+      >
+        <div className="px-5 pb-5">
+          <h3 className="text-gray-900 font-semibold text-2xl tracking-tight dark:text-white">
+            {props.lecturer.name} {props.lecturer.surname}
+          </h3>
 
-    return (
-        <>
-            <div
-                onClick={() => navigate(`/lecturers/${props.lecturer.slug}`)}
-                className="bg-gray-100 shadow-2xl rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700 dark:shadow-2xl cursor-pointer py-3 px-3"
-            >
-                <div className="px-5 pb-5">
-                    <h3 className="text-gray-900 font-semibold text-2xl tracking-tight dark:text-white">
-                        {props.lecturer.name}
-                    </h3>
+          <div className="flex items-center mt-2.5 mb-5 dark:text-white">
+            {props.lecturer.description}
+          </div>
 
-                    <div className="flex items-center mt-2.5 mb-5 dark:text-white">
-                        {props.lecturer.description}
-                    </div>
+          <div className="flex items-center justify-between">
+            <span className="text-xl font-bold text-gray-900 dark:text-white mt-5">
+              Email: {props.lecturer.email}
+            </span>
+          </div>
 
-                    <div className="flex items-center justify-between">
-                      <span className="text-xl font-bold text-gray-900 dark:text-white mt-5">
-                        Email: {props.lecturer.email}
-                      </span>
-                    </div>
-
-                    <div
-                        onClick={() => navigate(`/lecturers/${props.lecturer.slug}`)}
-                        className="dark:text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-5"
-                    >
-                        Види повеќе
-                    </div>
-
-                </div>
-            </div>
-        </>
-    )
-}
+          <div
+            onClick={() => navigate(`/profile/${props.lecturer.username}`)}
+            className="dark:text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-5"
+          >
+            Види повеќе
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Lecturer;
--- coursesApp/src/Pages/Lecturers/LecturersPage.jsx
+++ coursesApp/src/Pages/Lecturers/LecturersPage.jsx
@@ -1,76 +1,108 @@
-import React, {useState} from "react";
-import {useParams} from "react-router-dom"
-import {Pagination} from "@mui/material";
-import Lecturer from '../../Components/Lecturer/Lecturer'
-import Header from '../../Components/Header/Header'
-import Footer from '../../Components/Footer/Footer'
-
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { CircularProgress, Pagination } from "@mui/material";
+import Lecturer from "../../Components/Lecturer/Lecturer";
+import Header from "../../Components/Header/Header";
+import Footer from "../../Components/Footer/Footer";
+import axios from "axios";
 
 const LecturersPage = () => {
+  const params = useParams();
+  const { category } = params;
+  const [lecturers, setLecturers] = useState([]);
+  const [page, setPage] = useState(1);
+  const [pages, setPages] = useState(1);
+  const [isLoading, setIsLoading] = useState("");
 
-    const params = useParams();
-    const {category} = params;
-
-    // PlaceHolder for a lecturers
-    const lecturer1 = {
-        id: 1,
-        slug: "lecturer#1",
-        name: "Dr. Evelyn Steele",
-        description: "Професорката Евелин Стил е истакнат член на факултетот на Универзитетот MIT, позната" +
-            "по своето искуство во областа на сајбер безбедноста. Со повеќе децении искуство во истражувањето и примената на сајбер безбедност, професорката" +
-            "Стил носи богатство на знаење и страст за образованието на својата улога како едукатор.",
-        email: "evelynsteele@gmail.com",
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get(`/api/user/lecturers?page=${page}`);
+        setLecturers(data.lecturers);
+        setPage(data.page);
+        setPages(data.pages);
+        setIsLoading(false);
+      } catch (error) {
+        console.log("Error fetching categories");
+      }
     };
-    const lecturer2 = {
-        id: 2,
-        slug: "lecturer#2",
-        name: "Dr. Benjamin Hughes",
-        description: "Професорот Бенџамин Хјуз е истакнат член на факултетот на Универзитетот MIT, позната" +
-            "по своето искуство во областа на сајбер безбедноста. Со повеќе децении искуство во истражувањето и примената на сајбер безбедност, професорката" +
-            "Стил носи богатство на знаење и страст за образованието на својата улога како едукатор.",
-        email: "benjaminhughes@yahoo.com",
-    };
-    const lecturer3 = {
-        id: 1,
-        slug: "lecturer#1",
-        name: "Dr. Evelyn Steele",
-        description: "Професорката Евелин Стил е истакнат член на факултетот на Универзитетот MIT, позната" +
-            "по своето искуство во областа на сајбер безбедноста. Со повеќе децении искуство во истражувањето и примената на сајбер безбедност, професорката" +
-            "Стил носи богатство на знаење и страст за образованието на својата улога како едукатор.",
-        email: "evelynsteele@gmail.com",
-    };
-    const lecturer4 = {
-        id: 1,
-        slug: "lecturer#1",
-        name: "Dr. Evelyn Steele",
-        description: "Професорката Евелин Стил е истакнат член на факултетот на Универзитетот MIT, позната" +
-            "по своето искуство во областа на сајбер безбедноста. Со повеќе децении искуство во истражувањето и примената на сајбер безбедност, професорката" +
-            "Стил носи богатство на знаење и страст за образованието на својата улога како едукатор.",
-        email: "evelynsteele@gmail.com",
-    };
+    fetchData();
+  }, [page]);
 
-    const [lecturers, setLecturers] = useState([lecturer1, lecturer2, lecturer3, lecturer4]);
+  const handlePageChange = (event, value) => {
+    setPage(value);
+  };
 
+  // PlaceHolder for a lecturers
+  const lecturer1 = {
+    id: 1,
+    slug: "lecturer#1",
+    name: "Dr. Evelyn Steele",
+    description:
+      "Професорката Евелин Стил е истакнат член на факултетот на Универзитетот MIT, позната" +
+      "по своето искуство во областа на сајбер безбедноста. Со повеќе децении искуство во истражувањето и примената на сајбер безбедност, професорката" +
+      "Стил носи богатство на знаење и страст за образованието на својата улога како едукатор.",
+    email: "evelynsteele@gmail.com",
+  };
+  const lecturer2 = {
+    id: 2,
+    slug: "lecturer#2",
+    name: "Dr. Benjamin Hughes",
+    description:
+      "Професорот Бенџамин Хјуз е истакнат член на факултетот на Универзитетот MIT, позната" +
+      "по своето искуство во областа на сајбер безбедноста. Со повеќе децении искуство во истражувањето и примената на сајбер безбедност, професорката" +
+      "Стил носи богатство на знаење и страст за образованието на својата улога како едукатор.",
+    email: "benjaminhughes@yahoo.com",
+  };
+  const lecturer3 = {
+    id: 1,
+    slug: "lecturer#1",
+    name: "Dr. Evelyn Steele",
+    description:
+      "Професорката Евелин Стил е истакнат член на факултетот на Универзитетот MIT, позната" +
+      "по своето искуство во областа на сајбер безбедноста. Со повеќе децении искуство во истражувањето и примената на сајбер безбедност, професорката" +
+      "Стил носи богатство на знаење и страст за образованието на својата улога како едукатор.",
+    email: "evelynsteele@gmail.com",
+  };
+  const lecturer4 = {
+    id: 1,
+    slug: "lecturer#1",
+    name: "Dr. Evelyn Steele",
+    description:
+      "Професорката Евелин Стил е истакнат член на факултетот на Универзитетот MIT, позната" +
+      "по своето искуство во областа на сајбер безбедноста. Со повеќе децении искуство во истражувањето и примената на сајбер безбедност, професорката" +
+      "Стил носи богатство на знаење и страст за образованието на својата улога како едукатор.",
+    email: "evelynsteele@gmail.com",
+  };
 
-    return (
-        <>
-            <Header page={"Lecturers"}/>
-            <div>
-                <h2 className="text-center mt-5 text-3xl">{category}</h2>
-                <div className="flex justify-around mt-5 w-11/12 m-auto">
-                    <div className="grid lg:grid-cols-2 md:grid-cols-2 gap-10 mt-5">
-                        {lecturers.map((l) => (
-                            <Lecturer key={l.id} lecturer={l}/>
-                        ))}
-                    </div>
-                </div>
-                <div className="mt-5 flex justify-center">
-                    <Pagination count={10} color="primary" className="mt-10 mb-10"/>
-                </div>
-            </div>
-            <Footer/>
-        </>
-    )
-}
+  return (
+    <>
+      <Header page={"Lecturers"} />
+      <div>
+        <h2 className="text-center mt-5 text-3xl">{category}</h2>
+        <div className="flex justify-around mt-5 w-11/12 m-auto">
+          <div className="grid lg:grid-cols-2 md:grid-cols-2 gap-10 mt-5">
+            {isLoading ? (
+              <CircularProgress size="1rem" color="inherit" />
+            ) : (
+              lecturers.map((l) => <Lecturer key={l.id} lecturer={l} />)
+            )}
+          </div>
+        </div>
+        <div className="mt-5 flex justify-center">
+          <Pagination
+            count={pages}
+            page={page}
+            onChange={handlePageChange}
+            color="primary"
+            className="mt-10 mb-10"
+          />
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
 export default LecturersPage;
